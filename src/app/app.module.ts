f0b1e0d85581ff43169d1ea1a6937d3d0e68a288
aesher9o1 <aashiskumar986@gmail.com>
2018-10-16 10:17:10 +0530

    enviroment not pushed and firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FirebaseModule, FirebaseProvider} from 'angular-firebase';
 import { AppComponent } from './app.component';
+import { environment } from './../environments/environment'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FirebaseModule
   ],
-  providers: [],
+  providers: [FirebaseProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
